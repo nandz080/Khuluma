64c7654683efcf3320d017fc8384b55a7fd7bb9d
nandz080 <nandz080@gmail.com>
2024-07-01 23:34:47 +0200

    feature: update settings.py for google signin functionality

--- Khuluma/urls.py
+++ Khuluma/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    #path('auth/', include('social_django.urls', namespace='social')),
     path('', include('user.urls')),  # Include other user URLs
 ]
 
