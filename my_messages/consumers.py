db8da6823987c6e1a05e39f753e22caa25df1fe5
nandz080 <nandz080@gmail.com>
2024-07-04 00:47:05 +0200

    Updaate: update routing.py and consumers.py to include websockets

--- my_messages/consumers.py
+++ my_messages/consumers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 from .models import Message
 from .serializers import MessageSerializer
 from asgiref.sync import async_to_sync
@@ -12,36 +12,43 @@ class ChatConsumer(WebsocketConsumer):
     It is responsible for handling incoming websocket messages and sending them to the 
     appropriate recipients.
     """
-    def connect(self):
+    async def connect(self):
         """
         Connects to the websocket and accepts the connection.
         """
-        self.accept()
-        self.group_name = 'chat'
-        async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
             self.channel_name
         )
 
-    def disconnect(self, close_code):
+        await self.accept()
+
+    async def disconnect(self, close_code):
         """
         Disconnects from the websocket and closes the connection.
         """
-        async_to_sync(self.channel_layer.group_discard)(
-            self.group_name,
+        await self.channel_layer.group_discard(
+            self.room_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         """
         Receives incoming websocket messages and handles them accordingly.
         """
-        data = json.loads(text_data)
-        action = data.get('action')
-        if action == 'send':
-            self.handle_send_message(data)
-        elif action == 'read':
-            self.handle_read_message(data)
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
 
     def handle_send_message(self, data):
         """
@@ -75,19 +82,29 @@ class ChatConsumer(WebsocketConsumer):
             'message_id': message_id
         }))
     
-    def chat_message(self, event):
+    async def chat_message(self, event):
+        """
+        Handles sending a message to a recipient.
+        """
         message = event['message']
-        self.send(text_data=json.dumps({
+
+        await self.send(text_data=json.dumps({
             'message': message
         }))
 
     def send_receipt(self, event):
+        """
+        Handles sending a send receipt to the sender.
+        """
         send_receipt = event['send_receipt']
         self.send(text_data=json.dumps({
             'send_receipt': send_receipt
         }))
 
     def read_receipt(self, event):
+        """
+        Handles sending a read receipt to the sender.
+        """
         read_receipt = event['read_receipt']
         self.send(text_data=json.dumps({
             'read_receipt': read_receipt
--- my_messages/routing.py
+++ my_messages/routing.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-from django.urls import path, re_path
-from .consumers import ChatConsumer
+
+from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/my_messages/', ChatConsumer.as_asgi()),
-   #re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
-
